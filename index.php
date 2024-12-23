803f75eb9c1a9766238a2376092669fefa16640d
Jerry <princessorjerry@gmail.com>
2024-12-23 09:16:06 +0700

    hi

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Code by thaibinhvip - https://www.facebook.com/botsim
+ * @Copyright S2VN.TOP
+ * Website: https://nam.name.vn
+ * 
+ */
+$key='M.S2VN.TOP wap Tiện Ích Mobi';
+$mkey='Wap Tiện ích mobi, Tổng hợp tool tiện ích wap web, tool leech , mod java online, wapftp, wap proxy, xem mã nguồn , ...';
+include 'template/header.php';?>
+<div class="phdr">Thông Tin Cập Nhật</div>			<div class="textwidget"><p><span style="color: #ff0000;"><strong>Lưu ý:</strong></span> Nên lưu lại logo sau khi tạo trên trang của bạn hay trên các hệ thống lưu trữ tập tin tránh dùng link trực tiếp của hệ thống.<br />
+Wapsite hiện đang được cập nhật... Chúc các bạn online vui vẻ! :)</p>
+</div>
+<div class="widget">
+<div class="phdr">Tiện ích</div>
+<div class="gmenu" style="text-align:center"><a href="http://mp3.s2vn.top" rel="menu" title="Nghe tai nhac mp3 chat luong cao mien phi">Tải Nhạc MP3</a> | <a href="http://tube.s2vn.top" rel="menu" title="tai video youtube nhac mien phi">Tải VIDEO</a> | <a href="http://haivl.s2vn.top/" rel="menu" title="Anh hai che anh haivl">Meme hài</a></div>
+<div class="list1"><font color=red>◆</font><a href="http://awap.cf" rel="nofollow"  title="hosting mien phi zingdata.ga">Free Web Hosting</a></div>
+<div class="list1"><font color=red>◆</font><a href="http://sim.s2vn.top" title="simsimi chat bot online, api simsimi">Chat With Simsimi Online</a></div>
+<div class="list1"><font color=red>◆</font><a href="http://proxy.s2vn.top" title="wap proxy fake ip">Wap Proxy Việt Nam</a></div>
+<div class="list1"><font color=red>◆</font><a href="https://video.s2vn.top" rel="menu" title="get link download youtube, youtube to mp3">Tải video từ YouTube</a></div>
+<div class="list1"><font color=red>◆</font><a href="http://tools.s2vn.top" rel="menu" title="webmaster tool seo, backlink">WebMaster Tools - Tools Seo Pro</a></div>
+</div>
+<div class="widget">
+<div class="phdr">Công Cụ Tổng Hợp</div>
+<div class="list1"><font color=red>◆</font><a href="/whois.php" rel="menu"  title="kiem tra ten mien whois">Kiểm Tra Domain - Whois </a></div>
+<div class="list1"><font color=red>◆</font><a href="/source.php" rel="menu" title="Soi Mã Nguồn Wap/Web">Soi Mã Nguồn Wap/Web</a></div>
+<div class="list1"><font color=red>◆</font><a href="/logo" rel="menu" title="tao logo wap web online">Tạo Logo Wap Online</a></div>
+<div class="list1"><font color=red>◆</font><a href="/chupanh" rel="menu" title="chup anh wap web online">Chụp Ảnh Wap/Web Online</a></div>
+<div class="list1"><font color=red>◆</font><a href="/imgur.php" rel="menu" title="upload anh len imgur">Lưu Trữ Ảnh Trên Imgur</a></div>
+<div class="list1"><font color=red>◆</font><a href="/mahoa.php" rel="menu" title="upload anh len imgur">Tool Mã hoá/Giải mã Tổng hợp</a></div>
+<div class="list1"><font color=red>◆</font><a href="/ddos.php" rel="menu" title="Tang traffic ao, visit cho wap/web">Tăng Visit cho trang Wap/Web</a></div>
+<div class="list1"><font color=red>◆</font><a href="/cloudflare.php" rel="menu"  title="Truy tim ip website su dung dns cloudflare">Kiểm Tra IP trang dùng DNS của CloudFlare </a></div>
+</div>
+<?php
+include 'code/list.php';
+include 'template/footer.php';
+?>
